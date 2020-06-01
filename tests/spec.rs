efabd057b3e459ead6503c819036ecfa2481526e
Ingvar Stepanyan <rreverser@google.com>
2020-06-01 17:00:01 +0100

    Switch to wast crate
    Use wast (Rust implementaton) instead of wabt bindings for parsing the test suite for easier integration / control.

--- tests/spec.rs
+++ tests/spec.rs
@@ -3,37 +3,12 @@ compile_error!("Tests must be run without lazy blobs.");
 
 use libtest_mimic::{run_tests, Arguments, Outcome, Test};
 use std::error::Error;
-use std::fs::{read, read_dir};
+use std::fs::{read_dir, read_to_string};
 use std::path::Path;
-use wabt::script::{Command, CommandKind, ScriptParser};
 use wasmbin::io::Decode;
 use wasmbin::module::Module;
-
-fn fmt_wabt_error(err: wabt::Error) -> String {
-    use wabt::ErrorKind;
-
-    match err.kind() {
-        ErrorKind::Nul => "string contained nul-byte".to_owned(),
-        ErrorKind::Deserialize(err) => format!("failed to deserialize:\n{:#}", err),
-        ErrorKind::Parse(err) => format!("failed to parse:\n{:#}", err),
-        ErrorKind::WriteText => "failed to write text".to_owned(),
-        ErrorKind::NonUtf8Result => "result is not a valid utf8".to_owned(),
-        ErrorKind::WriteBinary => "failed to write binary".to_owned(),
-        ErrorKind::ResolveNames(err) => format!("failed to resolve names:\n{:#}", err),
-        ErrorKind::Validate(err) => format!("failed to validate:\n{:#}", err),
-    }
-}
-
-fn fmt_wabt_script_error(err: wabt::script::Error) -> String {
-    match err {
-        wabt::script::Error::IoError(err) => format!("{}", err),
-        wabt::script::Error::WabtError(err) => format!("Wabt: {}", fmt_wabt_error(err)),
-        wabt::script::Error::Other(err) => format!("{}", err),
-        wabt::script::Error::WithLineInfo { line, error } => {
-            format!("at line {}: {}", line, fmt_wabt_script_error(*error))
-        }
-    }
-}
+use wast::parser::{parse, ParseBuffer};
+use wast::Wast;
 
 struct WasmTest {
     module: Vec<u8>,
@@ -41,29 +16,42 @@ struct WasmTest {
 }
 
 fn read_tests(path: &Path, dest: &mut Vec<Test<WasmTest>>) -> Result<(), Box<dyn Error>> {
-    let src = read(path)?;
-    let mut features = wabt::Features::new();
-    features.enable_all();
-    let mut parser =
-        ScriptParser::<f32, f64>::from_source_and_name_with_features(&src, "test.wast", features)
-            .map_err(fmt_wabt_script_error)?;
-    while let Some(Command { kind, line }) = parser.next().map_err(fmt_wabt_script_error)? {
-        let (module, expect_result) = match kind {
-            CommandKind::AssertMalformed { module, message } => (module, Err(message)),
-            CommandKind::Module { module, .. } => (module, Ok(())),
+    let src = read_to_string(path)?;
+    let set_err_path_text = |mut err: wast::Error| {
+        err.set_path(path);
+        err.set_text(&src);
+        err
+    };
+    let buf = ParseBuffer::new(&src).map_err(&set_err_path_text)?;
+    let wast = parse::<Wast>(&buf).map_err(&set_err_path_text)?;
+    for directive in wast.directives {
+        let (span, mut module, expect_result) = match directive {
+            wast::WastDirective::AssertMalformed {
+                span,
+                module: wast::QuoteModule::Module(module),
+                message,
+            } => (span, module, Err(message)),
+            wast::WastDirective::Module(module) => (module.span, module, Ok(())),
             _ => {
                 // Skipping interpreted
                 continue;
             }
         };
+        let (line, col) = span.linecol_in(&src);
         dest.push(Test {
-            name: format!("{}:{}", path.display(), line),
+            name: format!("{}:{}:{}", path.display(), line, col),
             kind: String::default(),
-            is_ignored: false,
+            is_ignored: match expect_result {
+                // Our low-level parser doesn't validate sections.
+                Err("function and code section have inconsistent lengths") => true,
+                // We intentionally read future table/memory IDs.
+                Err("zero flag expected") => true,
+                _ => false,
+            },
             is_bench: false,
             data: WasmTest {
-                module: module.into_vec(),
-                expect_result,
+                module: module.encode()?,
+                expect_result: expect_result.map_err(|err| err.to_owned()),
             },
         });
     }
