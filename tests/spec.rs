21effe13b768f9f2665063f5e0f3d92c75bd6f3d
Ingvar Stepanyan <rreverser@google.com>
2020-06-01 13:45:28 +0100

    Use libtest-mimic as a test runner

--- tests/spec.rs
+++ tests/spec.rs
@@ -1,12 +1,13 @@
 #[cfg(feature = "lazy-blob")]
 compile_error!("Tests must be run without lazy blobs.");
 
+use libtest_mimic::{run_tests, Arguments, Outcome, Test};
+use std::error::Error;
 use std::fs::{read_dir, read_to_string};
 use std::path::Path;
-use std::error::Error;
+use wabt::script::{Command, CommandKind, ScriptParser};
 use wasmbin::io::Decode;
 use wasmbin::module::Module;
-use wabt::script::{Command, CommandKind, ScriptParser};
 
 fn fmt_wabt_error(err: wabt::Error) -> String {
     use wabt::ErrorKind;
@@ -28,58 +29,78 @@ fn fmt_wabt_script_error(err: wabt::script::Error) -> String {
         wabt::script::Error::IoError(err) => format!("{}", err),
         wabt::script::Error::WabtError(err) => format!("Wabt: {}", fmt_wabt_error(err)),
         wabt::script::Error::Other(err) => format!("{}", err),
-        wabt::script::Error::WithLineInfo { line, error } => format!("at line {}: {}", line, fmt_wabt_script_error(*error)),
+        wabt::script::Error::WithLineInfo { line, error } => {
+            format!("at line {}: {}", line, fmt_wabt_script_error(*error))
+        }
     }
 }
 
-fn check_wast(path: &Path) -> Result<(), Box<dyn Error>> {
-    let path = path.as_os_str().to_str().ok_or("Invalid UTF-8 path")?;
+struct WasmTest {
+    module: Vec<u8>,
+    expect_result: Result<(), String>,
+}
+
+fn read_tests(path: &Path, dest: &mut Vec<Test<WasmTest>>) -> Result<(), Box<dyn Error>> {
     let src = read_to_string(path)?;
     let mut parser = ScriptParser::<f32, f64>::from_str(&src).map_err(fmt_wabt_script_error)?;
     while let Some(Command { kind, line }) = parser.next().map_err(fmt_wabt_script_error)? {
-        let (module, expect_res) = match kind {
-            CommandKind::AssertMalformed { module, message } => {
-                (module, Err(message))
-            }
-            CommandKind::Module { module, .. } => {
-                (module, Ok(()))
-            }
+        let (module, expect_result) = match kind {
+            CommandKind::AssertMalformed { module, message } => (module, Err(message)),
+            CommandKind::Module { module, .. } => (module, Ok(())),
             _ => {
                 // Skipping interpreted
-                continue
+                continue;
             }
         };
-        println!("Test {}:{}", path, line);
-        println!("---");
-        match (Module::decode(&mut module.into_vec().as_slice()), expect_res) {
-            (Ok(_), Err(err)) => println!("Expected an invalid module definition at line {} with an error: {}", line, err),
-            (Err(err), Ok(())) => println!("Expected a valid module definition at line {}. Error:\n{:#}", line, err),
-            _ => println!("OK"),
-        }
-        println!("===");
+        dest.push(Test {
+            name: format!("{}:{}", path.display(), line),
+            kind: String::default(),
+            is_ignored: false,
+            is_bench: false,
+            data: WasmTest {
+                module: module.into_vec(),
+                expect_result,
+            },
+        });
     }
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    for file in read_dir("tests/testsuite")? {
+    let mut tests = Vec::new();
+    for file in read_dir(Path::new("tests").join("testsuite"))? {
         let path = file?.path();
         if !path.extension().map_or(false, |ext| ext == "wast") {
             continue;
         }
-        match check_wast(&path) {
-            Ok(()) => {},
-            Err(err) => {
-                // If an error comes from the Wabt, ignore for now - most likely it uses a new feature we don't support yet.
-                let _ = err;
-                /*
-                println!("Suite {}", path.display());
-                println!("---");
-                println!("{}", err);
-                println!("===");
-                */
-            }
+        if let Err(err) = read_tests(&path, &mut tests) {
+            // If an error comes from the Wabt, ignore for now - most likely it uses a new feature we don't support yet.
+            eprintln!("Could not read test {}: {}", path.display(), err);
         }
     }
-    Ok(())
+    assert!(
+        !tests.is_empty(),
+        "Couldn't find any tests. Did you run `git submodule update --init`?"
+    );
+    run_tests(&Arguments::from_args(), tests, |test| {
+        match (
+            Module::decode(&mut test.data.module.as_slice()),
+            &test.data.expect_result,
+        ) {
+            (Ok(_), Err(err)) => Outcome::Failed {
+                msg: Some(format!(
+                    "Expected an invalid module definition with an error: {}",
+                    err
+                )),
+            },
+            (Err(err), Ok(())) => Outcome::Failed {
+                msg: Some(format!(
+                    "Expected a valid module definition, but got an error:\n{:#}",
+                    err
+                )),
+            },
+            _ => Outcome::Passed,
+        }
+    })
+    .exit()
 }
