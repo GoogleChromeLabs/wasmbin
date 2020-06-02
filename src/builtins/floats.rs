a651bde7007e60d009ac94ab4f8a37893e6c5b23
Ingvar Stepanyan <rreverser@google.com>
2020-06-02 15:43:04 +0100

    Use macro for floats

--- src/builtins/floats.rs
+++ src/builtins/floats.rs
@@ -1,34 +1,25 @@
 use crate::io::{Decode, DecodeError, Encode};
 use crate::visit::Visit;
 
-impl Encode for f32 {
-    fn encode(&self, w: &mut impl std::io::Write) -> std::io::Result<()> {
-        w.write_all(&self.to_bits().to_le_bytes())
-    }
+macro_rules! def_float {
+    ($ty:ident) => {
+        impl Encode for $ty {
+            fn encode(&self, w: &mut impl std::io::Write) -> std::io::Result<()> {
+                w.write_all(&self.to_le_bytes())
+            }
+        }
+
+        impl Decode for $ty {
+            fn decode(r: &mut impl std::io::Read) -> Result<Self, DecodeError> {
+                let mut bytes = [0; std::mem::size_of::<Self>()];
+                r.read_exact(&mut bytes)?;
+                Ok($ty::from_le_bytes(bytes))
+            }
+        }
+
+        impl Visit for $ty {}
+    };
 }
 
-impl Decode for f32 {
-    fn decode(r: &mut impl std::io::Read) -> Result<Self, DecodeError> {
-        let mut bytes = [0; 4];
-        r.read_exact(&mut bytes)?;
-        Ok(f32::from_bits(u32::from_le_bytes(bytes)))
-    }
-}
-
-impl Visit for f32 {}
-
-impl Encode for f64 {
-    fn encode(&self, w: &mut impl std::io::Write) -> std::io::Result<()> {
-        w.write_all(&self.to_bits().to_le_bytes())
-    }
-}
-
-impl Decode for f64 {
-    fn decode(r: &mut impl std::io::Read) -> Result<Self, DecodeError> {
-        let mut bytes = [0; std::mem::size_of::<f64>()];
-        r.read_exact(&mut bytes)?;
-        Ok(f64::from_bits(u64::from_le_bytes(bytes)))
-    }
-}
-
-impl Visit for f64 {}
+def_float!(f32);
+def_float!(f64);
